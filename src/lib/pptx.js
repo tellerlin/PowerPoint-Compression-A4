6df144867f3d5d73626df4dd24044213a4fc8028
tellerlin <lintao_mailbox@163.com>
2024-12-13 20:54:55 +0800

    优化4

--- src/lib/pptx.js
+++ src/lib/pptx.js
@@ -10,14 +10,13 @@ export async function optimizePPTX(file, options = {}) {
     const data = await file.async('uint8array');
     zip.file(mediaPath, await compressImage(data));
   }
-  return await zip.generateAsync({ type: 'blob', compression: 'DEFLATE', compressionOptions: { level: 9 } });
+  return await zip.generateAsync({type: 'blob',compression: 'DEFLATE',compressionOptions: {level: 9}});
 }
-
 async function compressImage(data) {
   const blob = new Blob([data]);
   const bitmap = await createImageBitmap(blob);
   const canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
   const ctx = canvas.getContext('2d');
   ctx.drawImage(bitmap, 0, 0);
-  return new Uint8Array(await (await canvas.convertToBlob({ type: 'image/jpeg', quality: 0.7 })).arrayBuffer());
+  return new Uint8Array(await (await canvas.convertToBlob({type: 'image/jpeg',quality: 0.7})).arrayBuffer());
 }
