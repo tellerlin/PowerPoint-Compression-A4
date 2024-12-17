8d9ae4c6f07d69ebafff20f28204cc21609cca41
tellerlin <lintao_mailbox@163.com>
2024-12-18 07:56:59 +0800

    增加社交媒体分享和google analytics和metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,40 @@
   import { Header } from '$lib/components/layout/Header';
   import { Footer } from '$lib/components/layout/Footer';
   import { Container } from '$lib/components/ui';
+  import { siteMetadata } from '$lib/config/metadata';
+  import { initializeGoogleAnalytics, initializeWeChatMetaTags } from '$lib/utils/analytics';
 
   onMount(() => {
     themeStore.initialize();
+    
+    setTimeout(() => {
+      initializeGoogleAnalytics();
+      initializeWeChatMetaTags(siteMetadata);
+    }, 2000);
   });
 </script>
+
+<svelte:head>
+  <title>{siteMetadata.title.en}</title>
+  <meta name="description" content={siteMetadata.description.en}>
+  <meta name="keywords" content={siteMetadata.keywords}>
+  <meta name="author" content={siteMetadata.author}>
+  <meta name="robots" content="index, follow">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="canonical" href={siteMetadata.url}>
+
+  <meta property="og:type" content="website">
+  <meta property="og:url" content={siteMetadata.url}>
+  <meta property="og:title" content={siteMetadata.title.en}>
+  <meta property="og:description" content={siteMetadata.description.en}>
+  <meta property="og:image" content="{siteMetadata.url}{siteMetadata.images.og}">
+
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:url" content={siteMetadata.url}>
+  <meta name="twitter:title" content={siteMetadata.title.en}>
+  <meta name="twitter:description" content={siteMetadata.description.en}>
+  <meta name="twitter:image" content="{siteMetadata.url}{siteMetadata.images.twitter}">
+</svelte:head>
 
 <div class="min-h-screen flex flex-col bg-background text-text">
   <Header />
