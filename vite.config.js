433c1965809525d6b8011e74f1454ec1b100ec4b
Lin <teller.lin@sap.com>
2025-04-10 09:41:57 +0800

    OK 修改config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    include: ['jszip']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/jszip/, /node_modules/]
+    }
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 });
