865a107807e876d022f59d18dc17cf1b2382071c
Lin <teller.lin@sap.com>
2025-04-20 10:30:12 +0800

    OK 优化代码1

--- src/lib/components/ui/index.js
+++ src/lib/components/ui/index.js
@@ -1,6 +1,5 @@
-export * from './Alert.svelte';
-export * from './Button.svelte';
-export * from './Card.svelte';
-export * from './Container.svelte';
-export * from './Input.svelte';
+export { default as Alert } from './Alert.svelte';
+export { default as Button } from './Button.svelte';
+export { default as Card } from './Card.svelte';
 export { default as Container } from './Container.svelte';
+export { default as Input } from './Input.svelte';
